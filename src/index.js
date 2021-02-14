cd804cfa62481eccd29212525ff5d163964b3344
heuzin <matheusqs.arq@gmail.com>
2021-02-14 15:08:54 -0300

    header object for title and subtitle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import ReactDOM from 'react-dom';
 
+const header = {
+    title: 'Indecision App',
+    subTitle: 'Put your life in the hands of a computer'
+}
 // JSX - JavaScript XML
  const template = (
     <div>
-        <h1>Indecision App</h1>
-        <p>This is some info</p>
+        <h1>{header.title}</h1>
+        <p>{header.subTitle}</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>
