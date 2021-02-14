100b85ff84568f7eb43351f9541e265e17987762
heuzin <matheusqs.arq@gmail.com>
2021-02-14 16:09:43 -0300

    conditional rendering for subtitle and for options array

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import ReactDOM from 'react-dom';
 
 const header = {
     title: 'Indecision App',
-    subTitle: 'Put your life in the hands of a computer'
+    subTitle: 'Put your life in the hands of a computer',
+    options: ['one', 'two', 'three']
 }
 // JSX - JavaScript XML
  const template = (
     <div>
         <h1>{header.title}</h1>
-        <p>{header.subTitle}</p>
+        {header.subTitle && <p>{header.subTitle}</p>}
+        {header.options.length > 0 ? header.options.map((option) => {
+           return <p>{option}</p>
+        }) : <p>No options</p>}
         <ol>
             <li>Item one</li>
             <li>Item two</li>
