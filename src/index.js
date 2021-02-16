38fecde25a8292099ffc8e5ff3a67e8e1a4f317a
heuzin <matheusqs.arq@gmail.com>
2021-02-16 20:59:41 -0300

    using state to save options

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 
 class IndecisionApp extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            options: ['thing one' , 'thing two', 'thing four']
+        }
+    }
     render() {
         const title = 'Indecision'
         const subtitle = 'Put your life in the hands of a computer'
-        const options = ['thing one' , 'thing two', 'thing four']
 
         return (
             <div>
                 <Header title={title} subtitle={subtitle} />
                 <Action />
-                <Options options={options} />
+                <Options options={this.state.options} />
                 <AddOption />
             </div>
         )
