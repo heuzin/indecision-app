dce2a26d7446e5c9bfb4c3dd89c86e7230cc142e
heuzin <matheusqs.arq@gmail.com>
2021-02-15 23:36:31 -0300

    initial fnction for clicking onMakeDecision button

--- src/index.js
+++ src/index.js
@@ -23,6 +23,12 @@ const onRemoveAll = () => {
     renderIndecisionApp()
 }
 
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum]
+    alert(option)
+}
+
 const appRoot = document.getElementById('app')
 
 const renderIndecisionApp = () => {
@@ -31,7 +37,7 @@ const renderIndecisionApp = () => {
             <h1>{app.title}</h1>
             {app.subTitle && <p>{app.subTitle}</p>}
             {app.options.length > 0 ? <p>Here are your options:</p> : <p>No options</p>}
-        
+            <button onClick={onMakeDecision}>What should I do?</button>
             <button onClick={onRemoveAll}>Remove All</button>
             <ol>
                 {app.options.map((option, i) => <li key={option[i]}>{option}</li>)}
