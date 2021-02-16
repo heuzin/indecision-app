c165d001a09f1a688a8d017378fc0dbfcc2aaa26
heuzin <matheusqs.arq@gmail.com>
2021-02-15 23:32:41 -0300

    setting up random option button

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const renderIndecisionApp = () => {
             <h1>{app.title}</h1>
             {app.subTitle && <p>{app.subTitle}</p>}
             {app.options.length > 0 ? <p>Here are your options:</p> : <p>No options</p>}
-            <p>{app.options.length}</p>
+        
             <button onClick={onRemoveAll}>Remove All</button>
             <ol>
                 {app.options.map((option, i) => <li key={option[i]}>{option}</li>)}
