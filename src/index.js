13a10dd4af17e8dee3821724cbd27516bd438b90
heuzin <matheusqs.arq@gmail.com>
2021-02-16 21:02:20 -0300

    disable action button if no options

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header title={title} subtitle={subtitle} />
-                <Action />
+                <Action hasOptions={this.state.options.length > 0} />
                 <Options options={this.state.options} />
                 <AddOption />
             </div>
@@ -41,7 +41,12 @@ class Action extends React.Component {
     render () {
         return (
             <div>
-                <button onClick={this.handlePick}>What Should I Do?</button>
+                <button 
+                    onClick={this.handlePick}
+                    disabled={!this.props.hasOptions}
+                >
+                    What Should I Do?
+                </button>
             </div>
         )
     }
