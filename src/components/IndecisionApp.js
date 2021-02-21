a79b69e5d4c5ba70f00fdeb749dfefdc7163a2c0
heuzin <matheusqs.arq@gmail.com>
2021-02-20 23:24:41 -0300

    container css for centering page and change background color for body

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -64,18 +64,20 @@ export default class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header title={title} subtitle={subtitle} />
-                <Action 
-                    hasOptions={this.state.options.length > 0} 
-                    handlePick={this.handlePick}
-                />
-                <Options 
-                    options={this.state.options} 
-                    handleDeleteOption={this.handleDeleteOption}
-                    handleDeleteOptions={this.handleDeleteOptions}
-                />
-                <AddOption 
-                    handleAddOption={this.handleAddOption}
-                />
+                <div className='container'>
+                    <Action 
+                        hasOptions={this.state.options.length > 0} 
+                        handlePick={this.handlePick}
+                    />
+                    <Options 
+                        options={this.state.options} 
+                        handleDeleteOption={this.handleDeleteOption}
+                        handleDeleteOptions={this.handleDeleteOptions}
+                    />
+                    <AddOption 
+                        handleAddOption={this.handleAddOption}
+                    />
+                </div>
                 <OptionModal
                     selectedOption={this.state.selectedOption}
                     handleClearSelectedOption={this.handleClearSelectedOption}
